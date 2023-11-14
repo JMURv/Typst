86ede535ca1143aad481de08bc10d36caa2137a7
JMURv <architect.lock@outlook.com>
2023-11-15 00:11:45 +0300

    test commit

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,21 @@
 echo "Stop containers"
 docker-compose down
 
-echo "Deleting images"
+echo "Deleting containers, images and volumes"
+docker rm "$(docker ps -q -f status=exited)"
+
 docker image rm typst-backend
 docker image rm typst-frontend
 docker image rm typst-recommends
+
+docker volume rm "$(docker volume ls -q --filter dangling=true)"
+docker volume rm node_modules_typst
 
 echo "Pull changes"
 git pull origin main
 
 echo "Start typst containers"
-docker-compose up
+docker-compose build --no-cache
+docker-compose up -d
 
 echo "Finish deploying!"
